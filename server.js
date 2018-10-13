ad0b4043a8ea81d72725a31886c1c274f6053257
srandle93 <randle.symone.l@gmail.com>
2018-10-13 12:24:54 -0500

    New Commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "app/public")));
+app.use(express.static("app/public"));
 
 // Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
 // Add the application routes
-require(path.join(__dirname, "app/routing/apiRoutes"))(app);
-require(path.join(__dirname, "app/routing/htmlRoutes"))(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 // Start listening on PORT
 app.listen(PORT, function() {
